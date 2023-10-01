76f8fbe3921b1eefca34e05b6b1094d5f6e3b739
Nirbhay Malhotra <nirbhaymalhotra2@gmail.com>
2023-10-01 19:23:29 -0400

    Second Page Activity

--- app/src/main/java/com/example/calculatorflashcardapp/MainActivity2.kt
+++ app/src/main/java/com/example/calculatorflashcardapp/MainActivity2.kt
@@ -2,10 +2,15 @@ package com.example.calculatorflashcardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        var generateButton = findViewById<Button>(R.id.generateId)
+        generateButton.setOnClickListener {
+
+        }
     }
 }
